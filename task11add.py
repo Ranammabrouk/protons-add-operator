918d3fea476b0cd7bd280dc440523ca09d375f5c
Rana Mabrouk <ranamabrouk@gmail.com>
2023-08-22 01:06:26 +0300

    ADD

---
+++ task11add.py
@@ -0,0 +1,6 @@
+operation=input()
+if operation=="1":
+    num1=input("enter first number")
+    num2=input("enter second number")
+    print ("the sum is"+ str(int(num1) + int(num2)))
+#code for add
